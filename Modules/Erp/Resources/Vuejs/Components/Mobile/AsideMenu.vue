9aeed1b5ecac6a6dd457965892f99b1ca6b69501
minh <nguyenminh902@gmail.com>
2023-07-31 16:14:27 +0700

    mobile erp

--- Modules/Erp/Resources/Vuejs/Components/Mobile/AsideMenu.vue
+++ Modules/Erp/Resources/Vuejs/Components/Mobile/AsideMenu.vue
@@ -2,22 +2,31 @@
 import { reactive, computed,ref, watch,onMounted, getCurrentInstance, onUpdated } from 'vue'
 import { usePage,Link, router } from '@inertiajs/vue3'
 import Menu from './Menu.vue'
+import UserProfile from './UserProfile.vue'
 let menu_bottom = reactive({})
-let show = ref(false)
+let showMenu = ref(false)
+let showUser = ref(false)
 
 menu_bottom = computed(() => usePage().props.navigation.menu_bottom)
-
 const internalInstance = getCurrentInstance()
 
 const handleDetailMenu = (value) => {
-    show.value = true
+   switch(value.summary) {
+      case 'user':
+        showUser.value = true
+        break;
+      case 'menu':
+        showMenu.value = true
+        break;
+    }
     document.querySelector('.bow-old').classList.remove('hidden')
     document.querySelector('.bow-old').classList.add('block')
     internalInstance.appContext.config.globalProperties.$overflowHidenBody()
 
 }
 const handleClose = () => {
-    show.value = false
+    showMenu.value = false
+    showUser.value = false
     document.querySelector('.bow-old').classList.remove('block')
     document.querySelector('.bow-old').classList.add('hidden')
     internalInstance.appContext.config.globalProperties.$removeOverflowHidenBody()
@@ -25,20 +34,29 @@ const handleClose = () => {
 }
 </script>
 <template>
-    <div class="fixed bottom-0 left-0 w-screen h-14 bg-white" v-click-outside="handleClose">
+    <div class="fixed bottom-0 left-0 w-screen h-14 bg-white" >
         <div class="flex items-center justify-center w-full h-full"
             :class="{
                 'px-4' : menu_bottom.length == 4
             }">
-            <div v-for="(value, key) in menu_bottom" :key="key" :class="'w-1\/'+menu_bottom.length" @click.prevent="handleDetailMenu(value)">
+            <div v-for="(value, key) in menu_bottom" :key="key" class="w-1/2" :class="'w-1/'+menu_bottom.length" @click.prevent="handleDetailMenu(value)">
                     <div class="text-center text-icon"><i :class="[value.icon, value.class]"></i></div>
                     <div class="text-center text-mini text-icon" >{{ value.name }}</div>
             </div>
         </div>
     </div>
-    <Transition name="slide-down" >
-       <Menu   v-if="show"></Menu>
-    </Transition >
+    <div>
+        <div class="absolute w-screen h-screen top-0 left-0 z-50" v-if="showMenu" @click="handleClose"></div>
+        <Transition name="slide-down" >
+           <Menu  v-if="showMenu"></Menu>
+        </Transition >
+    </div>
+    <div>
+        <div class="absolute w-screen h-screen top-0 left-0 z-50" v-if="showUser" @click="handleClose"></div>
+        <Transition name="slide-down" >
+           <UserProfile   v-if="showUser"></UserProfile>
+        </Transition >
+    </div>
 </template>
 <style type="text/css">
 </style>
--- Modules/Erp/Resources/Vuejs/Components/Mobile/Menu.vue
+++ Modules/Erp/Resources/Vuejs/Components/Mobile/Menu.vue
@@ -3,6 +3,7 @@ import { reactive, computed,ref, watch,onMounted, getCurrentInstance } from 'vue
 import { usePage,Link, router } from '@inertiajs/vue3'
 
 const checkActive = ref(router.page.url)
+
 let menu = reactive({})
 menu = computed(() => usePage().props.navigation.menu_module)
 const checkExpanded = (childs) => {
@@ -18,11 +19,18 @@ const checkExpanded = (childs) => {
 </script>
 <template>
     <div class="absolute bg-white w-full rounded-t-xl bottom-0 left-0 z-50 p-4">
-        <div class="flex items-center py-2" :class="{'text-main' : checkExpanded(value.children),}" v-for="(value, key) in menu" :key="key">
-            <i :class="value.icon" class="mr-3"></i>
-            <div >
-                {{ value.name }}
+        <div class="" v-for="(value, key) in menu" :key="key">
+            <div class="flex items-center py-2" :class="{'text-main' : checkExpanded(value.children)}">
+                <i :class="value.icon" class="mr-3"></i>
+                <div>
+                    {{ value.name }}
+                </div>
             </div>
+           <ul class="pl-6" v-if="checkExpanded(value.children)">
+               <li v-for="(child, i) in value.children" :key="i" class="p-2 rounded-md" :class="{ 'text-main bg-bghover' : checkActive == child.link}">
+                   <a href="" >{{ child.name }}</a>
+               </li>
+           </ul>
         </div>
     </div>
 </template>
